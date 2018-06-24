058a75fc4d18f3651d65cc0023307e16ec079479
github username <schun08@gmail.com>
2018-06-24 11:36:56 -0400

    test Venue component to make sure it renders properly

---
+++ client/src/components/Venue.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Venue extends Component {
+  render() {
+    return (
+      <div className="venue-component">
+        <p>this is a test of the Venue component</p>
+      </div>
+    )
+  }
+}
