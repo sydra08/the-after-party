084d66642d45123810a4c58508a751fccefd9046
github username <schun08@gmail.com>
2018-06-24 11:29:21 -0400

    add address to Suggestion Serializer

--- app/serializers/suggestion_serializer.rb
+++ app/serializers/suggestion_serializer.rb
@@ -1,5 +1,5 @@
 class SuggestionSerializer < ActiveModel::Serializer
-  attributes :id, :name, :category_id, :upvotes, :downvotes
+  attributes :id, :name, :category_id, :upvotes, :downvotes, :address_id
   belongs_to :venue, serializer: VenueSerializer
   belongs_to :address, serializer: AddressSerializer
   belongs_to :category, serializer: CategorySerializer
