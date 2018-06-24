f11593f8af6ba572e0a59edc1fad5b7652790faa
github username <schun08@gmail.com>
2018-06-24 11:33:16 -0400

    fix issue where timestamps appeared in location info

--- app/serializers/address_serializer.rb
+++ app/serializers/address_serializer.rb
@@ -1,4 +1,4 @@
 class AddressSerializer < ActiveModel::Serializer
-  attributes :id, :street, :location
+  attributes :id, :street, :location_id
   belongs_to :location, serializer: LocationSerializer
 end
