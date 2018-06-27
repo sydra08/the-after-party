4683ea41348b4bb0afcaca2ce27530a2fd1adf63
github username <schun08@gmail.com>
2018-06-27 19:49:58 -0400

    set up and test default state of Venue component

--- client/src/components/Venue.js
+++ client/src/components/Venue.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 export default class Venue extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "testing",
+      location: "a place",
+      rating: "4"
+    };
+  }
+  componentDidMount() {
+    // fetch data
+  }
   render() {
     return (
       <div className="venue-component">
         <p>this is a test of the Venue component</p>
+        <h1>{this.state.name}</h1>
+        <h2>{this.state.location}</h2>
+        <h3>{this.state.rating}</h3>
       </div>
     )
   }
