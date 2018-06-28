de9ca30e15f82729be61f146ba73a945d116818b
github username <schun08@gmail.com>
2018-06-27 22:09:21 -0400

    set up related venue components. need to fix issue with data not saving in state properly

--- client/src/components/Venue.js
+++ client/src/components/Venue.js
@@ -1,29 +1,42 @@
 import React, { Component } from 'react';
 
-export default class Venue extends Component {
+const Venue = ({ name, avg_rating }) => {
+  return (
+    <div className="venue">
+      <h3>Name: {name}</h3>
+      <h4>Rating: {avg_rating}</h4>
+    </div>
+  )
+}
+
+const VenueList = ({ venues }) => (
+  <div className="venue-list">
+    {venues.map(venue => <Venue name="venue.name" rating="venue.avg_rating" />)}
+  </div>
+);
+
+
+export default class VenueContainer extends Component {
+  // need to create this as the venue list component because the data returned needs to be iterated over
   constructor() {
     super();
     this.state = {
-      name: "testing",
-      location: "a place",
-      rating: "4"
+      venues: []
     };
   }
-  // waits until after the component has updated to update the state with new data
+
   componentDidMount() {
     fetch('/venues')
       .then(response => response.json())
       .then(json => console.log(json))
+      .then(json => this.setState({venues: json}))
   }
-
 
   render() {
     return (
-      <div className="venue-component">
-        <p>this is a test of the Venue component</p>
-        <h1>{this.state.name}</h1>
-        <h2>{this.state.location}</h2>
-        <h3>{this.state.rating}</h3>
+      <div className="venue-container-component">
+        <p>this is a test of the Venue list component</p>
+        <VenueList venues={this.state.venues} />
       </div>
     )
   }
