d7b4019d3c0ef14304f7c14383ed469d46a1f39a
github username <schun08@gmail.com>
2018-07-01 17:16:04 -0400

    add NavBar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home.js'
+import NavBar from './components/NavBar.js'
 import VenueContainer from './components/containers/VenueContainer.js';
 import SuggestionContainer from './components/containers/SuggestionContainer.js';
 import ReviewContainer from './components/containers/ReviewContainer.js';
@@ -12,6 +13,7 @@ class App extends Component {
     return (
       <Router>
         <div>
+          <NavBar />
           <Route exact path="/" component={Home} />
           <Route exact path="/venues" component={VenueContainer} />
           <Route exact path="/suggestions" component={SuggestionContainer} />
