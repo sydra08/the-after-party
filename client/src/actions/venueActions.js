3b7569189c75800fb0f03315e4a71fc3930958bd
github username <schun08@gmail.com>
2018-07-01 18:43:32 -0400

    create actions directory. create venueActions file with fetchVenues()

---
+++ client/src/actions/venueActions.js
@@ -0,0 +1,12 @@
+import fetch from 'isomorphic-fetch';
+
+export function fetchVenues() {
+  return function(dispatch) {
+    return fetch('/venues')
+      .then(response => {
+        return response.json()
+      }).then(responseJSON => {
+        return responseJSON.images
+      }).then(venues => dispatch({type: 'FETCH_VENUES', payload: venues}))
+  }
+}
