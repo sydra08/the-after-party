749d8aa459dc58ac485d738166ee659a4e157fe6
github username <schun08@gmail.com>
2018-07-02 19:49:59 -0400

    use index instead of venue.id for key in VenueList

--- client/src/components/venues/VenueList.js
+++ client/src/components/venues/VenueList.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const VenueList = ({ venues }) => (
   <div className="venue-list">
     <ul>
-      {venues.map((venue) => <li key={venue.id}><Link style={{ marginRight: '12px' }} to={`/venues/${venue.id}`}>{venue.name}</Link></li>)}
+      {venues.map((venue, index) => <li key={index}><Link style={{ marginRight: '12px' }} to={`/venues/${venue.id}`}>{venue.name}</Link></li>)}
     </ul>
   </div>
 );
