6af98dc43244b3dda6c992d10e573e03544adf72
github username <schun08@gmail.com>
2018-07-02 19:53:59 -0400

    comment out hardcoded venues data in venueActions

--- client/src/actions/venueActions.js
+++ client/src/actions/venueActions.js
@@ -1,21 +1,21 @@
 import fetch from 'isomorphic-fetch';
 
 export function fetchVenues() {
-  const venues = [
-    {name: "Red Rocks Amphitheater", address_id: 1, avg_rating: 5, capacity: 9525},
-    {name: "Brooklyn Steel", address_id: 2, avg_rating: 3.5, capacity: 1800},
-    {name: "Madison Square Garden", address_id: 3, avg_rating: 4, capacity: 20789},
-    {name: "Irving Plaza", address_id: 4, avg_rating: 4, capacity: 1025}
-  ];
-  console.log("fetchVenues() was called")
-  return {
-    type: 'FETCH_VENUES',
-    payload: venues
-  }
-  // return function(dispatch) {
-  //   return fetch('/venues')
-  //     .then(response => {
-  //       return response.json()
-  //     }).then(venues => dispatch({type: 'FETCH_VENUES', payload: venues}))
+  // const venues = [
+  //   {name: "Red Rocks Amphitheater", address_id: 1, avg_rating: 5, capacity: 9525},
+  //   {name: "Brooklyn Steel", address_id: 2, avg_rating: 3.5, capacity: 1800},
+  //   {name: "Madison Square Garden", address_id: 3, avg_rating: 4, capacity: 20789},
+  //   {name: "Irving Plaza", address_id: 4, avg_rating: 4, capacity: 1025}
+  // ];
+  // console.log("fetchVenues() was called")
+  // return {
+  //   type: 'FETCH_VENUES',
+  //   payload: venues
   // }
+  return function(dispatch) {
+    return fetch('/venues')
+      .then(response => {
+        return response.json()
+      }).then(venues => dispatch({type: 'FETCH_VENUES', payload: venues}))
+  }
 };
