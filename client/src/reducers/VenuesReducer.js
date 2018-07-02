218a0ed927cf3a76eb066681622449a1d20be432
github username <schun08@gmail.com>
2018-07-01 21:20:54 -0400

    add logging to VenuesReducer

--- client/src/reducers/VenuesReducer.js
+++ client/src/reducers/VenuesReducer.js
@@ -1,8 +1,11 @@
 import { FETCH_VENUES } from '../actions/venueActions';
 
 function VenuesReducer(state = {loading: false, venues:[]}, action) {
+  console.log(action);
   switch(action.type){
     case 'FETCH_VENUES':
+      console.log("FETCH_VENUES was called")
+      console.log(state)
       return {loading: false, venues: action.payload};
     default:
       return state;
