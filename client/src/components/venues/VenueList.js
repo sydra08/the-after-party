8adb871872ac284cdfd26c2d86105c6d770f46d5
github username <schun08@gmail.com>
2018-07-03 18:36:43 -0400

    change format of VenueList to use return statement

--- client/src/components/venues/VenueList.js
+++ client/src/components/venues/VenueList.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const VenueList = ({ venues }) => (
-  <div className="venue-list">
-    <ul>
-      {venues.map((venue, index) => <li key={index}><Link style={{ marginRight: '12px' }} to={`/venues/${venue.id}`}>{venue.name}</Link></li>)}
-    </ul>
-  </div>
-);
+const VenueList = ({ venues }) => {
+  return (
+    <div className="venue-list">
+      <ul>
+        {venues.map((venue, index) => <li key={index}><Link style={{ marginRight: '12px' }} to={`/venues/${venue.id}`}>{venue.name}</Link></li>)}
+      </ul>
+    </div>
+  );
+};
 
 export default VenueList;
