ee258b331444577ba25ab631f909a29b3020fb2a
github username <schun08@gmail.com>
2018-07-03 20:23:49 -0400

    update VenuePage to not fetch venues again. still need to fix issue with data not coming through properly

--- client/src/containers/VenuePage.js
+++ client/src/containers/VenuePage.js
@@ -7,43 +7,51 @@ import SuggestionList from '../components/suggestions/SuggestionList';
 
 class VenuePage extends Component {
 
-  componentDidMount() {
-    this.props.fetchVenues()
-  }
+  // componentDidMount() {
+  //   if ( !this.props.venues.length ) {
+  //     this.props.fetchVenues();
+  //   }
+  // }
 
-  // figure out which venue it is and then get the suggestions for it (i.e. venues.venueId.suggestions)
-  selectVenue() {
-    console.log(this.props.match.params.venueId)
-    return this.props.venues.filter(v => v.id === this.props.match.params.venueId)
-  }
-
+  // need to figure out why this doesn't return the venue from the props
+  // selectVenue() {
+  //   console.log(this.props.match.params.venueId)
+  //   return this.props.venues.find(v => v.id === parseInt(this.props.match.params.venueId,10)[0])
+  // }
+  //
+  // <VenueShow venue={venue} />
+  // <SuggestionList suggestions={venue.suggestions} />
+  // according to the console.log when you render the component this.props.venues has 4 items
+  // but when you try to find the correct one it comes up empty even though the numbers are correct
   render() {
-    const venue = this.selectVenue()
+    // const venue = this.selectVenue()
     console.log(this.props.match.url)
     console.log(`props:`);
     console.log(this.props.venues)
     console.log('state')
     console.log(this.state)
-    console.log(venue)
+    console.log(this.props.venue)
     return (
       <div className="venue-container-component">
-        <VenueShow venue={venue} />
-        <SuggestionList suggestions={venue.suggestions} />
+        <VenueShow venue={this.props.venue} />
       </div>
     )
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    venues: state.venues
-  };
+const mapStateToProps = (state, ownProps) => {
+  const venue = state.venues.find(v => v.id === ownProps.match.params.venueId);
+  if (venue){
+    return { venue }
+  } else {
+    return {venue: {}}
+  }
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({fetchVenues: fetchVenues}, dispatch);
-};
+// const mapDispatchToProps = (dispatch) => {
+//   return bindActionCreators({fetchVenues: fetchVenues}, dispatch);
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(VenuePage);
+export default connect(mapStateToProps)(VenuePage);
 
 // export default VenuePage;
