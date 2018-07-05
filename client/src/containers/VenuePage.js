5d3442733b57ff0e3ac5685d2f012e6ec8e344c4
github username <schun08@gmail.com>
2018-07-05 16:59:18 -0400

    add Switch component to handle VenueList vs VenueShow components

--- client/src/containers/VenuePage.js
+++ client/src/containers/VenuePage.js
@@ -5,6 +5,15 @@ import VenueShow from './containers/VenueShow';
 
 // this contains the routes for the venue list and venue show components
 
-export default class VenuePage extends Component {
-
+const VenuePage = () => {
+  return(
+    <div>
+      <Switch>
+        <Route exact path='/venues' component={VenueList} />
+        <Route path='/venues/:venueId' component={VenueShow} />
+      </Switch>
+    </div>
+  )
 }
+
+export default VenuePage;
