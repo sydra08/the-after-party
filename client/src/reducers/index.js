de38c6adf7f3af9886fde72522862b55a182390c
github username <schun08@gmail.com>
2018-07-05 13:27:10 -0400

    add ReviewsReducer to RootReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import VenuesReducer from './VenuesReducer.js';
+import ReviewsReducer from './ReviewsReducer.js';
 
 const RootReducer = combineReducers({
-  venues: VenuesReducer
+  venues: VenuesReducer,
+  reviews: ReviewsReducer
 });
 
 export default RootReducer;
