e9c95aff0f3772d77cab62e189d28a18907e59fa
github username <schun08@gmail.com>
2018-07-05 21:39:29 -0400

    import font into index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import RootReducer from './reducers/index';
 import registerServiceWorker from './registerServiceWorker';
+import 'typeface-roboto';
 
 const store = createStore(
   RootReducer,
