294ce5341fd67c18b53ef3b0f0325a908a05cd36
github username <schun08@gmail.com>
2018-07-05 22:19:43 -0400

    use Card component for VenueItem

--- client/src/components/venues/VenueItem.js
+++ client/src/components/venues/VenueItem.js
@@ -1,16 +1,44 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
-const divStyle = {
-  border: '1px solid'
-}
+const styles = {
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    marginBottom: 16,
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
 
-const VenueItem = ({ venue }) => {
+const VenueItem = (props) => {
+  const { venue, classes } = props;
   return (
-    <div className="venue" style={divStyle}>
-      <h3>Name: {venue.name}</h3>
-      <p>Rating: {venue.avg_rating}</p>
+    <div className="venue">
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography variant="headline" component="h2">
+            Name: {venue.name}
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            Rating: {venue.avg_rating}
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   )
 }
 
-export default VenueItem;
+export default withStyles(styles)(VenueItem);
+// export default VenueItem;
