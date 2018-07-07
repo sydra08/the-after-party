8b3b02e1b042194be93d4860feda4803d23a819f
github username <schun08@gmail.com>
2018-07-07 13:10:56 -0400

    update Suggestion serializer to make address info available

--- app/serializers/suggestion_serializer.rb
+++ app/serializers/suggestion_serializer.rb
@@ -1,5 +1,5 @@
 class SuggestionSerializer < ActiveModel::Serializer
-  attributes :id, :name, :category_id, :upvotes, :downvotes, :address_id
+  attributes :id, :name, :category_id, :upvotes, :downvotes, :address
   belongs_to :venue, serializer: VenueSerializer
   belongs_to :address, serializer: AddressSerializer
   belongs_to :category, serializer: CategorySerializer
