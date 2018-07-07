5848460dab9779095267ae4195c6e54eaf7f7264
github username <schun08@gmail.com>
2018-07-07 18:52:09 -0400

    define custom association writers for address and category

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -4,4 +4,13 @@ class Suggestion < ApplicationRecord
   belongs_to :address
 
   scope :by_venue, ->(venue_id) { where(venue: venue_id) }
+
+  def address_attributes=(address_attributes)
+    self.address = Address.find_or_create_by(street: address_attributes[:street])
+    self.address.update(city: address_attributes[:city], state: address_attributes[:state])
+  end
+
+  def category_attributes=(category_attributes)
+    self.category = Category.find_or_create_by(name: category_attributes[:name])
+  end
 end
