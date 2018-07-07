b2b360a7ca0202e53f9124a941b494a88e9aa611
github username <schun08@gmail.com>
2018-07-07 13:10:43 -0400

    update Suggestion components to use Paper and Card styling

--- client/src/components/suggestions/Suggestion.js
+++ client/src/components/suggestions/Suggestion.js
@@ -1,13 +1,48 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
-const Suggestion = ({ name, category, upvotes, downvotes }) => {
+const styles = {
+  card: {
+    minWidth: 275,
+    marginBottom: 12,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    marginBottom: 16,
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
+
+
+const Suggestion = (props) => {
+  const { name, category, address, upvotes, downvotes, classes } = props;
   return (
     <div className="suggestion">
-      <h4>Name: {name}</h4>
-      <p>Category: {category}</p>
-      <p>Upvotes: {upvotes} | Downvotes: {downvotes}</p>
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography variant="headline" component="h2">
+            Name: {name}
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            Address: {address.street} | Category: {category}
+          </Typography>
+          <Typography component="p">
+           Upvotes: {upvotes} | Downvotes: {downvotes}
+         </Typography>
+        </CardContent>
+      </Card>
     </div>
   )
 }
 
-export default Suggestion;
+export default withStyles(styles)(Suggestion);
--- client/src/components/suggestions/SuggestionList.js
+++ client/src/components/suggestions/SuggestionList.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import Suggestion from './Suggestion.js';
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
-const SuggestionList = ({ suggestions }) => {
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+});
+
+const SuggestionList = (props) => {
+  const { suggestions, classes } = props;
   return(
-    <div className="suggestion-list">
-      <h3>Suggestions:</h3>
-      {suggestions.map((suggestion, index) => <Suggestion key={index} name={suggestion.name} upvotes={suggestion.upvotes} downvotes={suggestion.downvotes} />)}
+    <div>
+      <Paper className={classes.root} elevation={1}>
+        <Typography variant="headline" component="h3">
+          Suggestions
+        </Typography>
+          {suggestions.map((suggestion, index) => <Suggestion key={index} name={suggestion.name} upvotes={suggestion.upvotes} downvotes={suggestion.downvotes} />)}
+      </Paper>
     </div>
   );
 }
 
-export default SuggestionList;
+export default withStyles(styles)(SuggestionList);
