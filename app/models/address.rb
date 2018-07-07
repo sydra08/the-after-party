7a7cea496b3eb02a34e051b84474f65d1bdd0c9c
github username <schun08@gmail.com>
2018-07-07 18:29:27 -0400

    update activerecord associations after Location was removed

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,4 @@
 class Address < ApplicationRecord
-  belongs_to :location
   has_one :venue
   has_one :suggestion
 end
--- app/models/location.rb
+++
@@ -1,5 +0,0 @@
-class Location < ApplicationRecord
-  has_many :addresses
-  has_many :venues, through: :addresses
-  has_many :suggestions, through: :addresses
-end
