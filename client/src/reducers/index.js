3917457ae757cf8913efc4c1b446f4a89a831779
github username <schun08@gmail.com>
2018-07-08 20:20:52 -0400

    update RootReducer to include SuggestionsReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import VenuesReducer from './VenuesReducer.js';
 import ReviewsReducer from './ReviewsReducer.js';
+import SuggestionsReducer from './SuggestionsReducer.js';
 
 const RootReducer = combineReducers({
   venues: VenuesReducer,
-  reviews: ReviewsReducer
+  reviews: ReviewsReducer,
+  suggestions: SuggestionsReducer
 });
 
 export default RootReducer;
