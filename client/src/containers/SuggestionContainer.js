50aa687ae7a560eaf8eb90cbdf21056bd1cfde52
github username <schun08@gmail.com>
2018-07-08 20:32:15 -0400

    update SuggestionContainer to handle suggestion data

--- client/src/containers/SuggestionContainer.js
+++ client/src/containers/SuggestionContainer.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchSuggestions } from '../actions/suggestionActions';
 import SuggestionList from '../components/suggestions/SuggestionList.js';
+import SuggestionForm from '../components/suggestions/SuggestionForm.js';
 
-export default class SuggestionContainer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      suggestions: []
-    };
-  }
+class SuggestionContainer extends Component {
 
   componentDidMount() {
-    fetch('/venues/4/suggestions')
-      .then(response => response.json())
-      // .then(json => console.log(json))
-      // remove this because it causes issues with data rendering
-      .then(json => this.setState({suggestions: json}))
+    this.props.fetchSuggestions(this.props.venueId);
   }
 
   render() {
     return (
       <div className="suggestion-container-component">
         <p>this is a test of the Suggestion container component</p>
-        <SuggestionList suggestions={this.state.suggestions} />
+        <SuggestionList suggestions={this.props.suggestions} />
       </div>
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    suggestions: state.suggestions.suggestions,
+    isLoading: state.suggestions.isLoading,
+    isError: state.suggestions.isError
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({fetchSuggestions: fetchSuggestions}, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SuggestionContainer);
