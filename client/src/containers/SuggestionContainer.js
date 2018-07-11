e34e06472bbe72cfb0cf4130678d27569ab7b3eb
github username <schun08@gmail.com>
2018-07-10 21:32:07 -0400

    update SuggestionForm to be purely presentational

--- client/src/containers/SuggestionContainer.js
+++ client/src/containers/SuggestionContainer.js
@@ -1,14 +1,109 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { fetchSuggestions } from '../actions/suggestionActions';
+import { fetchSuggestions, addSuggestion } from '../actions/suggestionActions';
 import SuggestionList from '../components/suggestions/SuggestionList.js';
-import SuggestionForm from './SuggestionForm.js';
+import SuggestionForm from '../components/suggestions/SuggestionList.js';
 
 class SuggestionContainer extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      category_attributes: {
+        name: "fast casual"
+      },
+      address_attributes: {
+        street: '',
+        city: '',
+        state: ''
+      },
+      upvote: 1,
+      downvote: 0,
+      // venue_id: this.props.venueId
+    };
+  }
 
   componentDidMount() {
     this.props.fetchSuggestions(this.props.venueId);
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("suggestion was submitted")
+    console.log(this.state);
+    this.props.addSuggestion(this.state);
+    this.emptyAddress = {
+      street: '',
+      city: '',
+      state: ''
+    }
+
+    this.emptyCategory = {
+      name: ''
+    }
+
+    this.setState({
+      name: '',
+      category_attributes: [Object.assign({}, this.emptyCategory)],
+      address_attributes: [Object.assign({}, this.emptyAddress)],
+      upvotes: '',
+      downvotes: '',
+      venue_id: this.props.venueId
+    });
+  }
+
+  handleNameChange = (event) => {
+    // this works fine
+    const value = event.target.value;
+
+    this.setState({
+      name: value,
+    });
+  }
+
+  handleStreetChange = (event) => {
+    // this only adds the last field you enter into state
+    // need to figure out how to deal with the nested attributes
+    // don't want to have an event handler for each property
+    const value = event.target.value;
+
+    this.setState(Object.assign({}, this.state, { address_attributes: { street: value, city: this.state.address_attributes.addressCity, state: this.state.address_attributes.addressState }}));
+  }
+
+  handleCityChange = (event) => {
+    // this only adds the last field you enter into state
+    // need to figure out how to deal with the nested attributes
+    // don't want to have an event handler for each property
+    const value = event.target.value;
+
+    this.setState(Object.assign({}, this.state, { address_attributes: { street: this.state.address_attributes.addressStreet, city: value, state: this.state.address_attributes.addressState }}));
+  }
+
+  handleStateChange = (event) => {
+    // this only adds the last field you enter into state
+    // need to figure out how to deal with the nested attributes
+    // don't want to have an event handler for each property
+    const value = event.target.value;
+
+    this.setState(Object.assign({}, this.state, { address_attributes: { street: this.state.address_attributes.addressStreet, city: this.state.address_attributes.addressCity,state: value }}));
+  }
+
+  handleVoteChange = (event) => {
+    // this works fine
+    const value = event.target.value;
+    if (value === "upvote"){
+      this.setState(Object.assign({}, this.state, {upvote: 1, downvote: 0}))
+    } else if (value === "downvote"){
+      this.setState(Object.assign({}, this.state, {upvote: 0, downvote: 1}))
+    }
+  }
+
+  handleCategoryChange = (event) => {
+    // this only sets a value if you choose something other than the default
+    const value = event.target.value;
+    this.setState(Object.assign({}, this.state, { category_attributes: {name: value}}))
   }
 
   render() {
@@ -16,7 +111,18 @@ class SuggestionContainer extends Component {
       <div className="suggestion-container-component">
         <p>this is a test of the Suggestion container component</p>
         <SuggestionList suggestions={this.props.suggestions} />
-        <SuggestionForm venueId={this.props.venueId}/>
+        <SuggestionForm
+          handleSubmit={this.handleSubmit}
+          name={this.state.name} handleNameChange={this.handleNameChange} addressStreet={this.state.address_attributes.street} handleStreetChange={this.handleStreetChange}
+          addressCity={this.state.address_attributes.city}
+          handleCityChange={this.handleCityChange}
+          addressState={this.state.address_attributes.state}
+          handleStateChange={this.handleStateChange}
+          category={this.state.category_attributes.name}
+          handleCategoryChange={this.handleCategoryChange}
+          vote={this.state.value}
+          handleVoteChange={this.handleVoteChange}
+           />
       </div>
     )
   }
@@ -31,7 +137,7 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({fetchSuggestions: fetchSuggestions}, dispatch);
+  return bindActionCreators({fetchSuggestions: fetchSuggestions, addSuggestion: addSuggestion}, dispatch);
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SuggestionContainer);
--- client/src/containers/SuggestionForm.js
+++ client/src/containers/SuggestionForm.js
@@ -1,199 +1,91 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { addSuggestion } from '../actions/suggestionActions';
-
+import React from 'react';
 
 // need to figure out how to handle the nested form parts - how do they get added to state and how does that translate into the json that's sent to the API?
 
-class SuggestionForm extends Component {
-  constructor() {
-    super();
+const SuggestionForm = (props) => {
+  console.log("SuggestionForm component")
+  console.log(props)
+  const { handleSubmit,name, handleNameChange, addressStreet, handleStreetChange, addressCity, handleCityChange, addressState,
+  handleStateChange, category, handleCategoryChange, vote,   handleVoteChange } = props;
 
-    this.state = {
-      name: '',
-      category_attributes: {
-        name: "fast casual"
-      },
-      address_attributes: {
-        street: '',
-        city: '',
-        state: ''
-      },
-      upvote: 1,
-      downvote: 0,
-      venue_id: this.props.venueId
-    };
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("suggestion was submitted")
-    console.log(this.state);
-    this.props.addSuggestion(this.state);
-    this.emptyAddress = {
-      street: '',
-      city: '',
-      state: ''
-    }
-
-    this.emptyCategory = {
-      name: ''
-    }
-
-    this.setState({
-      name: '',
-      category_attributes: [Object.assign({}, this.emptyCategory)],
-      address_attributes: [Object.assign({}, this.emptyAddress)],
-      upvotes: '',
-      downvotes: '',
-      venue_id: this.props.venueId
-    });
-  }
-
-  handleNameChange = (event) => {
-    // this works fine
-    const value = event.target.value;
-    const name = event.target.name;
-
-    this.setState({
-      name: value,
-    });
-  }
-
-  handleStreetChange = (event) => {
-    // this only adds the last field you enter into state
-    // need to figure out how to deal with the nested attributes
-    // don't want to have an event handler for each property
-    const value = event.target.value;
-    const name = event.target.name;
-
-    this.setState(Object.assign({}, this.state, { address_attributes: { street: value, city: this.state.address_attributes.city, state: this.state.address_attributes.state }}));
-  }
-
-  handleCityChange = (event) => {
-    // this only adds the last field you enter into state
-    // need to figure out how to deal with the nested attributes
-    // don't want to have an event handler for each property
-    const value = event.target.value;
-    const name = event.target.name;
-
-    this.setState(Object.assign({}, this.state, { address_attributes: { street: this.state.address_attributes.street, city: value, state: this.state.address_attributes.state }}));
-  }
-
-  handleStateChange = (event) => {
-    // this only adds the last field you enter into state
-    // need to figure out how to deal with the nested attributes
-    // don't want to have an event handler for each property
-    const value = event.target.value;
-    const name = event.target.name;
-
-    this.setState(Object.assign({}, this.state, { address_attributes: { street: this.state.address_attributes.street, city: this.state.address_attributes.city,state: value }}));
-  }
-
-  handleVoteChange = (event) => {
-    // this works fine
-    const value = event.target.value;
-    if (value === "upvote"){
-      this.setState(Object.assign({}, this.state, {upvote: 1, downvote: 0}))
-    } else if (value === "downvote"){
-      this.setState(Object.assign({}, this.state, {upvote: 0, downvote: 1}))
-    }
-  }
-
-  handleCategoryChange = (event) => {
-    // this only sets a value if you choose something other than the default
-    const value = event.target.value;
-    this.setState(Object.assign({}, this.state, { category_attributes: {name: value}}))
-  }
-
-  render() {
-    console.log("SuggestionForm state...")
-    console.log(this.state)
-    return (
-      <form onSubmit={this.handleSubmit}>
+  return (
+    <form onSubmit={this.handleSubmit}>
+      <div>
+        <label>
+          Name:
+          <input
+            id="suggestion-name"
+            name="name"
+            type="text"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </label>
+      </div>
+      <fieldset>
+        <legend>Address</legend>
         <div>
           <label>
-            Name:
+            Street:
             <input
-              id="suggestion-name"
-              name="name"
+              id="suggestion-address-street"
+              name="street"
               type="text"
-              value={this.state.name}
-              onChange={(event) => this.handleNameChange(event)}
+              value={addressStreet}
+              onChange={handleStreetChange}
             />
           </label>
         </div>
-        <fieldset>
-          <legend>Address</legend>
-          <div>
-            <label>
-              Street:
-              <input
-                id="suggestion-address-street"
-                name="street"
-                type="text"
-                value={this.state.address_attributes.street}
-                onChange={(event) => this.handleStreetChange(event)}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              City:
-              <input
-                id="suggestion-address-city"
-                name="city"
-                type="text"
-                value={this.state.address_attributes.city}
-                onChange={(event) => this.handleCityChange(event)}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              State:
-              <input
-                id="suggestion-address-state"
-                name="state"
-                type="text"
-                value={this.state.address_attributes.state}
-                onChange={(event) => this.handleStateChange(event)}
-              />
-            </label>
-          </div>
-        </fieldset>
         <div>
           <label>
-            Category:
-            <select value={this.state.category_attributes.name} onChange={(event) => this.handleCategoryChange(event)}>
-              <option value="fast casual">Fast Casual</option>
-              <option value="beer bar">Beer Bar</option>
-              <option value="cocktail bar">Cocktail Bar</option>
-              <option value="fast food">Fast Food</option>
-            </select>
+            City:
+            <input
+              id="suggestion-address-city"
+              name="city"
+              type="text"
+              value={addressCity}
+              onChange={handleCityChange}
+            />
           </label>
         </div>
         <div>
           <label>
-            Upvote/Downvote:
-            <select value={this.state.value} onChange={(event) => this.handleVoteChange(event)}>
-              <option value="upvote">Like</option>
-              <option value="downvote">Dislike</option>
-            </select>
+            State:
+            <input
+              id="suggestion-address-state"
+              name="state"
+              type="text"
+              value={addressState}
+              onChange={handleStateChange}
+            />
           </label>
         </div>
-        <div>
-          <button type="submit">Submit Suggestion</button>
-        </div>
-      </form>
-    );
-  }
+      </fieldset>
+      <div>
+        <label>
+          Category:
+          <select value={category} onChange={handleCategoryChange}>
+            <option value="fast casual">Fast Casual</option>
+            <option value="beer bar">Beer Bar</option>
+            <option value="cocktail bar">Cocktail Bar</option>
+            <option value="fast food">Fast Food</option>
+          </select>
+        </label>
+      </div>
+      <div>
+        <label>
+          Upvote/Downvote:
+          <select value={vote} onChange={handleVoteChange}>
+            <option value="upvote">Like</option>
+            <option value="downvote">Dislike</option>
+          </select>
+        </label>
+      </div>
+      <div>
+        <button type="submit">Submit Suggestion</button>
+      </div>
+    </form>
+  );
 }
 
-// export default SuggestionForm;
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({addSuggestion: addSuggestion}, dispatch);
-};
-
-export default connect(null, mapDispatchToProps)(SuggestionForm);
+export default SuggestionForm;
