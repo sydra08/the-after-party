c608417dbe9e9e9253490825922bc6449614f64a
github username <schun08@gmail.com>
2018-07-10 21:04:05 -0400

    add addSuggestion() to suggestionActions. add ADD_SUGGESTION case to SuggestionsReducer

--- client/src/actions/suggestionActions.js
+++ client/src/actions/suggestionActions.js
@@ -8,3 +8,27 @@ export function fetchSuggestions(venueId) {
       .then(suggestion => dispatch({type: 'RECEIVED_SUGGESTIONS', payload: suggestion}))
   }
 }
+
+export function addSuggestion(suggestionData) {
+  return (dispatch) => {
+    // debugger
+    fetch(`/venues/${suggestionData.venue_id}/suggestions`, {
+    method: 'POST',
+    body: JSON.stringify(reviewData),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(function(response) {
+      if(response.status >= 400){
+        console.log('Error:')
+        console.log(response.status)
+        return response.json()
+      }
+      console.log('Success')
+      console.log(response.status)
+      return response.json()
+    })
+    .then(suggestion => dispatch({type: 'ADD_SUGGESTION', payload: suggestion}))
+  }
+}
--- client/src/reducers/SuggestionsReducer.js
+++ client/src/reducers/SuggestionsReducer.js
@@ -1,5 +1,15 @@
 function SuggestionsReducer(state = {suggestions: [], isLoading: false, isError: false}, action) {
   switch(action.type){
+    case 'ADD_SUGGESTION':
+      console.log('ADD_SUGGESTION was called. state is...')
+      console.log(state)
+      console.log('new state is')
+      console.log(action.payload)
+      if(action.payload.status >= 400) {
+        return Object.assign({}, state, { isError: true })
+      } else {
+        return Object.assign({}, state, { suggestions: state.suggestions.concat(action.payload), isLoading: false, isError: false })
+      }
     case 'FETCH_SUGGESTIONS':
       console.log("FETCH_SUGGESTIONS was called")
       console.log("state is...")
