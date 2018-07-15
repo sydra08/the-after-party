ca5e626f792bce5d7b810229a5673c1e092f16b9
github username <schun08@gmail.com>
2018-07-15 19:46:02 -0400

    update suggestion serializer to include venue_id

--- app/serializers/suggestion_serializer.rb
+++ app/serializers/suggestion_serializer.rb
@@ -1,5 +1,5 @@
 class SuggestionSerializer < ActiveModel::Serializer
-  attributes :id, :name, :category, :upvotes, :downvotes, :address
+  attributes :id, :name, :category, :upvotes, :downvotes, :address, :venue_id
   belongs_to :venue, serializer: VenueSerializer
   belongs_to :address, serializer: AddressSerializer
   belongs_to :category, serializer: CategorySerializer
