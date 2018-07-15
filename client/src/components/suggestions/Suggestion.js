9a5aa4a7771c367736fcd7876f9311009c92941b
github username <schun08@gmail.com>
2018-07-15 17:27:30 -0400

    add like/dislike buttons and divider to suggestion cards

--- client/src/components/suggestions/Suggestion.js
+++ client/src/components/suggestions/Suggestion.js
@@ -3,8 +3,17 @@ import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import ThumbDownIcon from "@material-ui/icons/ThumbDown";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 
-const styles = {
+const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+  },
   card: {
     minWidth: 275,
     marginBottom: 12,
@@ -20,15 +29,21 @@ const styles = {
   },
   pos: {
     marginBottom: 12,
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit,
+    fontSize: 14,
   }
-};
-
+});
 
 const Suggestion = (props) => {
   console.log("Suggestion component")
   const { name, category, address, upvotes, downvotes, classes } = props;
   return (
-    <div className={"suggestion"}>
+    <div className={classes.root}>
       <Card className={classes.card}>
         <CardContent>
           <Typography variant="title" gutterBottom>
@@ -42,10 +57,19 @@ const Suggestion = (props) => {
           </Typography>
           <Typography component="p">
              Upvotes: {upvotes}
-           </Typography>
-         <Typography component="p">
+          </Typography>
+          <Typography component="p" className={classes.pos}>
             Downvotes: {downvotes}
           </Typography>
+          <Divider />
+          <Button variant="contained" color="primary" className={classes.button}>
+            <ThumbUpIcon className={classes.leftIcon} />
+            Like
+          </Button>
+          <Button variant="contained" color="primary" className={classes.button}>
+            <ThumbDownIcon className={classes.leftIcon} />
+            Dislike
+          </Button>
         </CardContent>
       </Card>
     </div>
