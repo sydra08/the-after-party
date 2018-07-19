9bd61912958394903fb24315c92cc532673a7e2b
github username <schun08@gmail.com>
2018-07-18 20:25:40 -0400

    add categoryActions and CategoriesReducer

---
+++ client/src/actions/categoryActions.js
@@ -0,0 +1,10 @@
+import fetch from 'isomorphic-fetch';
+
+export function fetchCategories() {
+  return function(dispatch) {
+    dispatch({type: 'FETCH_CATEGORIES'})
+    return fetch('/categories')
+      .then(response => response.json())
+      .then(categories => dispatch({type: 'RECEIVED_CATEGORIES', payload: categories}))
+  }
+};
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import VenuesReducer from './VenuesReducer.js';
 import ReviewsReducer from './ReviewsReducer.js';
 import SuggestionsReducer from './SuggestionsReducer.js';
+import CategoriesReducer from './CategoriesReducer.js';
 
 const RootReducer = combineReducers({
   venues: VenuesReducer,
   reviews: ReviewsReducer,
-  suggestions: SuggestionsReducer
+  suggestions: SuggestionsReducer,
+  categories: CategoriesReducer
 });
 
 export default RootReducer;
