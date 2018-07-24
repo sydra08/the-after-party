2febde61da999de392d2ca2f3bed7b571a4eb70e
github username <schun08@gmail.com>
2018-07-23 20:52:12 -0400

    make the homepage image into a background and crop to fit on page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import '../App.css';
 
 const Home = () => {
   return (
     <div className="home">
-      <h1>Welcome to The After Party App</h1>
-      <img src="/applause-audience-band-196652.jpg" alt="concert pic"/>
+      <div id="banner">
+        <h1>The After Party</h1>
+      </div>
       <p>include a short description of the site - similar to beer me</p>
       <p>Have links to the venues list and the suggestions list</p>
       <p>Images: <a href="https://www.pexels.com/">Pexels</a></p>
