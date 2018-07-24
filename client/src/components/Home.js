a5b1218725ce760738efbab2e92c6f2bd20422c9
github username <schun08@gmail.com>
2018-07-23 20:41:05 -0400

    move image to /public. fix issue with it not displaying on page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
+
 const Home = () => {
   return (
     <div className="home">
       <h1>Welcome to The After Party App</h1>
-      <p>add an image here</p>
-      <img src="../images/applause-audience-band-196652.jpg" alt="concert pic"/>
+      <img src="/applause-audience-band-196652.jpg" alt="concert pic"/>
       <p>include a short description of the site - similar to beer me</p>
       <p>Have links to the venues list and the suggestions list</p>
       <p>Images: <a href="https://www.pexels.com/">Pexels</a></p>
