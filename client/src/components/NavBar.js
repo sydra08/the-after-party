6620b1924078fed975f34d772505c509ab4fd075
github username <schun08@gmail.com>
2018-12-08 17:33:08 -0500

    remove commented out code from NavBar component

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -31,43 +31,4 @@ function NavBar(props) {
   );
 }
 
-// ButtonAppBar.propTypes = {
-//   classes: PropTypes.object.isRequired,
-// };
-
 export default withStyles(styles)(NavBar);
-
-
-//
-// const link = {
-//   width: '100px',
-//   padding: '12px',
-//   margin: '0 6px 6px',
-//   background: 'blue',
-//   textDecoration: 'none',
-//   color: 'white',
-// }
-
-// const NavBar = () => {
-//   return (
-//     <div className="navbar">
-//       <NavLink
-//         to="/"
-//         exact
-//         style={link}
-//         activeStyle={{
-//           background: 'darkblue'
-//         }}
-//       >Home</NavLink>
-//       <NavLink
-//         to="/venues"
-//         style={link}
-//         activeStyle={{
-//           background: 'darkblue'
-//         }}
-//       >Venues</NavLink>
-//     </div>
-//   );
-// };
-
-// export default NavBar;
