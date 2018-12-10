2da7d709ab138b6d473c6a1d6488dd7504267964
github username <schun08@gmail.com>
2018-12-10 18:59:48 -0500

    remove unused update method

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -13,15 +13,6 @@ class VenuesController < ApplicationController
     end
   end
 
-  def update
-    @venue = Venue.find_by(id: params[:id])
-    if @venue.update(venue_params)
-      render json: @venue, status: 200
-    else
-      render json: @venue, status: 404
-    end
-  end
-
   private
 
   def venue_params
