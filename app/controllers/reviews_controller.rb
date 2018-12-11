b86fb14a27e74127a7e762fb2f506c8f08ab1a5e
github username <schun08@gmail.com>
2018-12-10 20:15:16 -0500

    add comments about #create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,6 +8,8 @@ class ReviewsController < ApplicationController
     @venue = Venue.find_by(id: params[:venue_id])
     @review = @venue.reviews.build(review_params)
     if @review.save
+      # if the new venue is successfully created, then update the rating for the venue and save
+      # the venue rating doesn't currently update when you add a new review bc the ReviewContainer only listens to the changes to reviews
       @venue.update_rating
       @venue.save
       # 201 means created
